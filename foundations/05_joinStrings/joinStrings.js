fd057d139f4daecc384d8bb6a94f020297d2338c
travitosama-tech <240234494+travitosama-tech@users.noreply.github.com>
2025-12-24 18:11:02 -0800

    solutions to exercise 5 in JS exercises. made proper changes and joined the strings together via instructions

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,8 +8,16 @@
 */
 
 // Add your code right below, good luck!
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
+greeting = "Hello! My name is "  + fullName + " and I am " + age + " years old."
 
+console.log(greeting);
 
 
 
