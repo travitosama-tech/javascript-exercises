f348c7df1ba2c841d88919fd90fd59df70d8752c
travitosama-tech <240234494+travitosama-tech@users.noreply.github.com>
2026-01-09 18:31:51 -0800

    solution to exercise 09 sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (isNaN(start) || isNaN(end)) return 'ERROR';
+    if (start < 0 || end < 0) return 'ERROR';
+    if (start > end) [start, end] = [end, start];
+    let sum = 0;
+        for (let j = start; j <= end; j++) {
+            sum += j;
+        }
+        return sum;
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
