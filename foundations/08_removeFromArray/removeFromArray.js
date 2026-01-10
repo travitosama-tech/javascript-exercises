323e420e5896a250106dcb5796b2c3a7c0ebf435
travitosama-tech <240234494+travitosama-tech@users.noreply.github.com>
2026-01-09 16:46:47 -0800

    solution to execise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const result = arr.filter(element => {
+        return !args.includes(element);
+    })
+    return result;
 };
 
 // Do not edit below this line
