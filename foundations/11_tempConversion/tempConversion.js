0275ab5fcd8637ffb30bd30d28cd6aff0dfe9efb
travitosama-tech <240234494+travitosama-tech@users.noreply.github.com>
2026-01-10 18:37:38 -0800

    solutions to exercise 11 mine could have probably been simplified to be a bit more readable reflecting on the answers

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let celsius = (temp - 32) * fraction.numerator / fraction.denominator
+    return Math.round(celsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const fraction = {
+  numerator: 5,
+  denominator: 9
 };
+
+const convertToFahrenheit = function(temp) {
+    let fahrenheit = (temp * (fractionTwo.numerator / fractionTwo.denominator) + 32)
+    return Math.round(fahrenheit * 10) / 10
+};
+
+const fractionTwo = {
+  numerator: 9,
+  denominator: 5
+};
+
+console.log(convertToFahrenheit(73.2));
 
 // Do not edit below this line
 module.exports = {
