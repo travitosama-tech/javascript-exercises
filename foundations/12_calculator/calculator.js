63cbd131ec218bf70c13f793d26034bf01044e32
travitosama-tech <240234494+travitosama-tech@users.noreply.github.com>
2026-02-10 15:14:55 -0800

    solutions to calculator exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = arr.reduce((sum, current) => sum + current, 0);
+  return result;
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  let result = arr.reduce((product, current) => product * current, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  if (exponent == 0) return 1;
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result; 
 };
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+	if (num < 0) return undefined;
+  if (num === 0) return 1;
+  let x = num - 1;
+  while (x > 1) {
+    num *= x;
+    x--;
+  }
+  return num;
 };
 
 // Do not edit below this line
