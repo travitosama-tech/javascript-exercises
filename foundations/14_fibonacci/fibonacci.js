62109c9954519f81fcf7110ec5380c35926dbe4d
travitosama-tech <240234494+travitosama-tech@users.noreply.github.com>
2026-02-10 18:30:31 -0800

    Update fibonacci.js

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = function() {
+const fibonacciSequence = [];
+const n = 10; // How many numbers to include in the sequence
 
+fibonacciSequence[0] = 0;
+fibonacciSequence[1] = 1;
+
+for (let i = 2; i < n; i++) {
+  fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+}
+
+console.log(fibonacciSequence);
 };
 
 // Do not edit below this line
